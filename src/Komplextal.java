f5d4d1a7c490f2270bd0a6282fb12d0460a29ed7
MilkyasEyob.Habte <milkyas23>
2020-02-07 16:19:01 +0100

    laggt metoder för additionen & multiplication.

--- src/Komplextal.java
+++ src/Komplextal.java
@@ -1,4 +1,5 @@
 import com.sun.org.apache.regexp.internal.RE;
+import com.sun.xml.internal.bind.WhiteSpaceProcessor;
 
 public class Komplex {
     double re;
@@ -6,14 +7,27 @@ public class Komplex {
 
     public Komplex(double r, double i) {
         this.re = r;
-        this.im = i:
+        this.im = i;
     }
     public static void main (String[] args) {
 
         Komplex a = new Komplex(6,8);
         Komplex b = new Komplex(5, 2);
         Komplex c = a.add(b);
-        Komplex d = a.multiply(b);
+        Komplex d = a. multiplay(b);
+    }
+
+
+    }
+
+    private Komplex add(Komplex b) {
+        return new Komplex(this.re+b.re, this.im+b.im);
+
+        private Komplex multiplay(Komplex b) {
+            return Komplex( re this.re-b.re, im this im*b.im );
+
+            //a.add(b)= ( Komplex a add Komplex b + im);
+
 
     }
 }
